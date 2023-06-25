4445aff24435d79d7e2d4c1e12d59b6003a1f3c9
sodai-watanabe <sodai@uniba.jp>
2023-06-25 23:58:54 +0900

    貪欲法。意外とできないもん

---
+++ 6-25/greedy/455.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int i,j,count;
+        i=0;
+        j=0;
+        count=0;
+
+        // 複数配列で、indexの進み方が異なる場合の実装
+        // while文で、条件は最大を超えない。中の条件分岐でindexをそれぞれ進める。
+        while (i<g.size() && j<s.size()) {
+            if (g.at(i) <= s.at(j)) {
+                i++;
+                j++;
+                count++;
+            } else {
+                j++;
+            }
+        }
+        return count;
+    }
+};
