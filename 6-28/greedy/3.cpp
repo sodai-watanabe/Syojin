4c404a6bd429bde0186aecc8da83ebdedcb919c0
sodai-watanabe <sodai@uniba.jp>
2023-06-29 06:44:42 +0900

    greedy.改善点のコメント

--- 6-28/greedy/3.cpp
+++ 6-28/greedy/3.cpp
@@ -7,6 +7,7 @@ int main () {
     int x;
     cin >> x;
     int y[4] = {50,10,5,1};   //具体的な成分が決まってる場合はvectorではなく、y[i]の形で宣言
+                              //const vector<int> coins = {50, 10, 5, 1};でもok
     vector<int>A;
 
     for (int i=0; i<4; i++) {
@@ -18,8 +19,8 @@ int main () {
     int ans = 0;
 
     for (int i=0; i<4; i++) {   //greedyに。
-        if (x/y[i] >= A.at(i)) {
-            x -= A.at(i)*(y[i]);
+        if (x/y[i] >= A.at(i)) {      //改善点：要するに、x/y[i]とA.at(i)の小さい方が、そのコインの枚数として採用されている。
+            x -= A.at(i)*(y[i]);      //つまり、条件分岐ではなく count = min(x/y[i], A.at(i)) でいける。
             ans += A.at(i);
         } else {
             int count = x/(y[i]);
